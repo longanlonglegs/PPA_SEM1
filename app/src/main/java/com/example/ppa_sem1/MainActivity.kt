7ddd415bbe41f18f74caf3c836f0ff35e406d877
longanlonglegs <loganwduran@gmail.com>
2025-02-15 23:27:43 +0800

    added size, but buggy when go to cart

--- app/src/main/java/com/example/ppa_sem1/MainActivity.kt
+++ app/src/main/java/com/example/ppa_sem1/MainActivity.kt
@@ -52,6 +52,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
